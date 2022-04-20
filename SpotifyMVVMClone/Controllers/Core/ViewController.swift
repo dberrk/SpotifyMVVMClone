a6623d37524ad92cfdc643f1a0c09572aeb74c9b
dberrk <dberkk1@gmail.com>
2022-04-20 16:20:29 +0300

    Initial commit

---
+++ SpotifyMVVMClone/Controllers/Core/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SpotifyMVVMClone
+//
+//  Created by Berk Macbook on 20.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        view.backgroundColor = .red
+        
+    }
+
+
+}
+
